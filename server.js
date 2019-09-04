f4de41995fa3e4657ae61a5f4c3a332f4782066e
Neha Patel <neha.r.p@gmail.com>
2019-09-04 15:53:01 -0700

    fixed logger function, now it is working

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const express = require("express");
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
 //custom middleware
 
 function logger(req, res, next) {
   console.log(
-    `[${new Date().toISOString}] ${req.method} to ${req.url} from ${req.get(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get(
       "Origin"
     )}`
   );
@@ -18,5 +14,9 @@ function logger(req, res, next) {
 }
 
 server.use(logger)
+
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
 
 module.exports = server;
